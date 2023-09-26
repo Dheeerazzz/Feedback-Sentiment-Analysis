605e49e83c29ef95b78bfb6b77689e2a1e3e4616
Mona Lisa <iamdheeraj2614@gmail.com>
2023-09-26 21:38:16 +0530

    spaces

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def home():
     bad_feedback = "We apologize for any inconvenience you experienced. Your feedback is important to us, and we'll take it into consideration to improve our services "
     sentiment = "Neutral"  
     scent="good"
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        sentiment_result = predict_sentiment(user_input)
-        if sentiment_result == [0]:
+    if request.method=='POST':
+        user_input=request.form['user_input']
+        sentiment_result=predict_sentiment(user_input)
+        if sentiment_result==[0]:
             sentiment = bad_feedback
             scent="bad"
         else:
